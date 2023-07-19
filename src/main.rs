72bbb71a098fa996bbd2c3171240467119bdd584
Charlie Wilson <charlie@spiralblue.space>
2023-07-20 01:17:34 +0200

    feat(*): add TODO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
+    // TODO: methods to:
+    //     - send message using api
+    //     - receive message using api
+    //     - get time remaining in session
+    //     - get startup command
+    //     - (ground) set startup command
+    //     - (ground) get time remaining in session (if not something that can be determined on board)
     println!("Hello, world!");
 }
